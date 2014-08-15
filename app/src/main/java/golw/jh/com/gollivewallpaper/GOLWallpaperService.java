76ab5de531ae728fe2aaf94b7ae857a120b9f46c
João Henriques <joaoh88@gmail.com>
2014-08-15 06:29:31 +0100

    3 clicks to reset, max 3 frames per second

--- app/src/main/java/golw/jh/com/gollivewallpaper/GOLWallpaperService.java
+++ app/src/main/java/golw/jh/com/gollivewallpaper/GOLWallpaperService.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Handler;
 import android.service.wallpaper.WallpaperService;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 
 import java.util.Random;
@@ -12,11 +13,15 @@ import java.util.Random;
 
 public class GOLWallpaperService extends WallpaperService {
 
+    private GOLState state = null;
 
     @Override
-    public Engine onCreateEngine() {
+    public Engine onCreateEngine()
+    {
+        state = null;
         return new DemoWallpaperEngine();
     }
+
 
     @Override
     public void onCreate() {
@@ -51,7 +56,7 @@ public class GOLWallpaperService extends WallpaperService {
             alive = new Paint();
             dead = new Paint();
 
-            alive.setColor(Color.WHITE);
+            alive.setColor(Color.DKGRAY);
             dead.setColor(Color.BLACK);
 
             int x,y;
@@ -59,8 +64,8 @@ public class GOLWallpaperService extends WallpaperService {
             for(y=0; y < h; y++)
             {
                 for(x=0; x < w; x++) {
-                    // has possibility of 1/10 to be alive
-                    curState[y][x] = ( r.nextInt(10) == 0 ) ;
+                    // has possibility of 1/5 to be alive
+                    curState[y][x] = ( r.nextInt(5) == 0 ) ;
                     //_temp   [y][x] = 0;
                 }
             }
@@ -114,16 +119,11 @@ public class GOLWallpaperService extends WallpaperService {
                     cur = getState(x,y);
                     neigh = countNeigh(x,y);
 
-                    // 1 is alive
-                    // 0 is dead
-                    // -1 no cell
                     if( cur )
                         _temp[y][x] = (neigh == 2 || neigh == 3) ;
 
                     else
                         _temp[y][x] = (neigh == 3) ;
-
-
 
 
                     if( FACTOR > 1 ) {
@@ -147,9 +147,6 @@ public class GOLWallpaperService extends WallpaperService {
     private class DemoWallpaperEngine extends Engine {
 
 
-        private GOLState state = null;
-
-
         private boolean mVisible = false;
         private final Handler mHandler = new Handler();
         private final Runnable mUpdateDisplay = new Runnable() {
@@ -157,6 +154,36 @@ public class GOLWallpaperService extends WallpaperService {
             public void run() {
                 draw();
             }};
+        private long lastTouchUp = 0;
+        private int touchCount = 0;
+
+
+        @Override
+        public void onTouchEvent(MotionEvent event) {
+
+            if( event.getAction() == MotionEvent.ACTION_UP )
+            {
+                //System.out.println("------------------------" + (System.currentTimeMillis() - lastTouchUp) );
+                if( lastTouchUp == 0 || ( System.currentTimeMillis() - lastTouchUp)  < 400 )
+                {
+                    lastTouchUp = System.currentTimeMillis();
+
+                    touchCount++;
+
+                    if( touchCount >= 2 )
+                    {
+                        state = null;
+                        touchCount = 0;
+                    }
+
+                    //System.out.println("------------------------------------" + touchCount);
+                }
+                else {
+                    touchCount = 0;
+                    lastTouchUp = 0;
+                }
+            }
+        }
 
         private void draw() {
             SurfaceHolder holder = getSurfaceHolder();
@@ -177,7 +204,7 @@ public class GOLWallpaperService extends WallpaperService {
 
             mHandler.removeCallbacks(mUpdateDisplay);
             if (mVisible) {
-                mHandler.postDelayed(mUpdateDisplay, 100);
+                mHandler.postDelayed(mUpdateDisplay, 333);
             }
         }
         @Override
